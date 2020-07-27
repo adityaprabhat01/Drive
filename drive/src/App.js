acee58abb62b58af3d1e73a21b9769fff4c12fde
Aditya Prabhat <adityaprabhat1998@gmail.com>
2020-07-27 23:54:35 +0530

    Landing Page

--- drive/src/App.js
+++ drive/src/App.js
@@ -5,11 +5,13 @@ import Drive from './components/Drive'
 import SignIn from './components/Auth/SignIn'
 import SignUp from './components/Auth/SignUp'
 import NonEmptyFolder from './components/folder/NonEmptyFolder'
+import Landing from './components/Landing'
 
 const App = () => {
   return (
       <BrowserRouter>
         <Switch>
+          <Route exact path="/" component={Landing} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
           <Route path='/drive' component={Drive} />
